d8a0e93d7ecca76438ad78cd25d0cbe558173dff
Maker3D 2 <Maker3D 2@UN2MAKER3D2.uniaraedu.local>
2026-06-11 10:54:15 -0300

    fix: corrigindo caminhos do tailwind

--- ideaforge/vite.config.js
+++ ideaforge/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './' // 👈 ISSO DAQUI garante que os estilos carreguem em QUALQUER lugar ou link da Vercel!
+  base: './' // 👈 ISSO DAQUI obriga o sistema a achar o Tailwind CSS em qualquer link!
 })
