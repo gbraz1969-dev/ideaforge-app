bbab4bbd0758c0d548d07492be6e2080a8ec50be
Maker3D 2 <Maker3D 2@UN2MAKER3D2.uniaraedu.local>
2026-06-11 15:06:55 -0300

    drible

--- ideaforge/src/App.jsx
+++ ideaforge/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Dashboard from './pages/Dashboard';
+import Landing from './pages/LandingPage';
+import Dashboard from './pages/DashboardPage';
 import { Toaster } from 'sonner';
 
 export default function App() {
