0359ddb48b9e2dfefcc908f114f0ca74698d2923
gbraz1969-dev <gbraz1969@gmail.com>
2026-06-11 17:09:33 -0300

    Refatoracao

--- ideaforge/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './' // 👈 ISSO DAQUI obriga o sistema a achar o Tailwind CSS em qualquer link!
-})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // 👈 ISSO DAQUI obriga o sistema a achar o Tailwind CSS em qualquer link!
 })
